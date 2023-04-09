ed7fd3cf6ba676cba86491135e291f28a7a830ab
Robson Junior <rob.santanaju@gmail.com>
2023-04-08 23:10:41 -0300

    api with bug and init client

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,25 @@
-import React, {useEffect, useState} from 'react'
+import logo from './logo.svg';
+import './App.css';
 
-function App()
-{
-  const [BackendData, setBackendData] = useState([{}])
-  
-  useEffect(() => 
-  {
-    fetch("api/").then(
-      response => response.json()
-    ).then(
-      data => (
-        setBackendData(data)
-      )
-    )
-  }, [])
-
-  return(
-    <div>
-      {(typeof BackendData.users === 'undefined') ? (
-      <p>Loading...</p>
-      ) : (
-        BackendData.users.map((user, i) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
-    </div> 
-  )
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
