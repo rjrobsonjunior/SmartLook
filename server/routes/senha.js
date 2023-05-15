b65128d2524d63b1bc132f7a51f621d5bfe0a410
Robson Junior <rob.santanaju@gmail.com>
2023-05-15 13:30:33 -0300

    login -> json(login and senha)

--- server/routes/senha.js
+++ server/routes/senha.js
@@ -4,16 +4,26 @@ import { db } from "../db.js";
 const router = express.Router();
 
 router.post('/login', (req, res) => {
+
     const login = req.body.login;
+    const senha1 = req.body.senha;
+
     const query = `SELECT senha FROM usuarios WHERE login = '${login}'`;
   
     db.query(query, (err, result) => {
       if (err) throw err;
       if (result.length === 0) {
         res.status(401).send('Login inválido!');
-      } else {
-        const senha = result[0].senha;
-        res.send(`Senha do usuário ${login}: ${senha}`);
+      } 
+      else {
+        if(senha1 == result[0].senha)
+        {
+          res.send(`true`);
+        }
+        else
+        {
+          res.send(`false`);
+        }
       }
     });
   });
