7201ce1fcde7fae4f2f2648e024416797cf27094
Robson Junior <rob.santanaju@gmail.com>
2023-06-26 09:41:11 -0300

    remove the number of people leaving

--- server/routes/entered.js
+++ server/routes/entered.js
@@ -14,6 +14,14 @@ router.post('/pessoas', (req, res) => {
     res.json({ success: true });
 });
 
+router.post('/pessoas-sairam', (req, res) => {
+  const { pessoas } = req.body;
+
+  numberP -= parseInt(pessoas);
+  console.log(numberP);
+  res.json({ success: true });
+});
+
 router.get('/setNumber', (req, res) => {
   res.json({number: numberP });
 });
